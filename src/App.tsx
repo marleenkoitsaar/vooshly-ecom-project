66cd849d034f787179b2712cd62ffb0fdc180ce3
marleenkoitsaar <marleenkoitsaar@gmail.com>
2024-03-10 20:43:30 +0200

    feat(checkout): add checkout and stripe

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { Outlet } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import CartDrawer from 'src/components/cartDrawer';
 import { CartProvider } from './context/cart';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const queryClient = new QueryClient();
 
@@ -15,6 +17,7 @@ function App() {
         <CartProvider>
           <Navbar />
           <CartDrawer />
+          <ToastContainer theme="dark" />
           <Box padding="4rem">
             <Outlet />
           </Box>
